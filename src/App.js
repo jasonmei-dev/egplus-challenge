b79ecf8e35aed353e5345fdbf4182a789ae3cb32
jmei403 <jason.mei403@gmail.com>
2021-05-17 10:54:10 -0400

    set up API request for users data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import './App.css';
 
-function App() {
+const App = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    generateUsers();
+  }, []);
+
+  const generateUsers = async () => {
+    const res = await axios.get('https://randomuser.me/api/?results=15&exc=login,info,registered,id&noinfo');
+
+    const data = res.data.results;
+    // console.log(res.data.results);
+    setUsers(data);
+  }
+
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <ul>
+        {users.map((user, index) => {
+          return <li key={index}>{user.email}</li>
+        })}
+      </ul>
     </div>
   );
 }
